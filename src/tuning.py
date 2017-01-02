af0e7b9b2c1d9a305ca6e365954b6cd4e8247ad0
martipa <martipa@student.ethz.ch>
2017-01-02 15:49:23 +0100

    modified tuning for multi-output

--- src/tuning.py
+++ src/tuning.py
@@ -1,36 +1,86 @@
 import numpy as np
 import scipy.io as sio
+import warnings
+import operator
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.grid_search import GridSearchCV
-from sklearn import cross_validation, metrics
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import hamming_loss, make_scorer
+from sklearn.pipeline import Pipeline
+from sklearn import metrics
+
+# testing
+from sklearn.feature_selection import SelectKBest
+
+# deprecation warnings
+warnings.simplefilter("ignore")
+
+# invert loss
+def neg_hamming_loss(y_true, y_pred):
+    return -hamming_loss(y_true, y_pred)
 
 def main():
 
-    features = 'X_17_20_2k'
+    features = 'X_7_40'
     path = 'features/'
 
     X_train = sio.loadmat(path+features+'_0.mat')[features+'_0']
     X_test = sio.loadmat(path+features+'_1.mat')[features+'_1']
-    Y = np.ravel(sio.loadmat(path+'Y.mat')['Y'])
+    Y = sio.loadmat(path+'Y.mat')['Y']
+
+    print('feature set', ':', features)
+    print('X_train', ':', X_train.shape)
+    print('Y', ':', Y.shape)
+
+    # == RandomForest ==
+    rfc = RandomForestClassifier()
 
     params = ({
-        'n_estimators' : [100],
-        'min_samples_split' : [3],
-        'min_samples_leaf' : [2],
-        'max_depth' : [6],
-        'max_features' : [200],
-        'class_weight' : [{0:3.0, 1:1}],
-        'random_state' : [19]
+        'n_estimators'      : [250],
+        'max_features'      : [500],
+        'class_weight'      : ['balanced'],
+        'max_depth'         : [3,5,7],
+        'min_samples_split' : [3,6,9],
+        'min_samples_leaf'  : [2,3,4],
+        'random_state'      : range(0,3),
     })
 
-    gsearch = GridSearchCV(estimator = RandomForestClassifier(), param_grid = params, scoring='neg_log_loss', n_jobs=-1, iid=False, cv=17)
+    gcv = GridSearchCV(estimator = rfc, param_grid = params, scoring=make_scorer(neg_hamming_loss), n_jobs=-1, iid=False, cv=17)
 
-    gsearch.fit(Xtrain, Y)
+    print('fitting...')
 
-    print(features)
-    print(gsearch.best_params_)
-    print(gsearch.best_score_)
+    gcv.fit(X_train, Y)
+
+    print('predicting...')
+
+    prd = gcv.predict(X_test)
+
+    if True:
+        print('==========================')
+        for i in prd:
+            print(i[0])
+            print(i[1])
+            print(i[2])
+
+    print('==========================')
+
+    for r in sorted(gcv.cv_results_.keys()):
+        if not r == 'params' and not r.startswith('split'):
+            print(r, ':', gcv.cv_results_[r])
+
+    print('params:')
+    for i in range(0, len(gcv.cv_results_['params'])):
+        print(gcv.cv_results_['params'][i], '| %.5f' % gcv.cv_results_['mean_test_score'][i], '| %.5f' % gcv.cv_results_['std_test_score'][i])
+
+    print('==========================')
+
+    print('cv mean', ':', gcv.best_score_)
+    print('cv std', ':', gcv.cv_results_['std_test_score'][gcv.best_index_])
+    print('feature set', ':', features)
+    for p in gcv.best_params_:
+        print(p, ':', gcv.best_params_[p])
 
 
 if __name__ == '__main__':
